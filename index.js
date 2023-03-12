dcfbd0c5ac5cb99f8441a53f3a1558a3c6c00376
RickVellingaa <rickvellinga2408@gmail.com>
2023-03-12 23:53:29 +0100

    start

---
+++ index.js
@@ -0,0 +1,43 @@
+// Importeer express uit de node_modules map
+import express from 'express'
+
+const url = 'https://api.vervoerregio-amsterdam.fdnd.nl/api/v1/principles'
+const data = await fetch(url).then((response) => response.json())
+
+// Maak een nieuwe express app aan
+const app = express()
+
+// Stel ejs in als template engine en geef de 'views' map door
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources
+app.use(express.static('public'))
+
+// Maak een route voor de index
+app.get('/', function (req, res) {
+  // res.send('Hello World!')
+  res.render('toolboard', data)
+})
+
+// app.get('/toolboard', function (req, res) {
+//   // res.send('Hello World!')
+//   res.render('toolboard', data)
+//   console.log(data);
+// })
+
+app.get('/contact', function (req, res) {
+  // res.send('Hello World!')
+  res.render('contact')
+})
+
+// Stel het poortnummer in waar express op gaat luisteren
+app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal het ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+console.log(data);
