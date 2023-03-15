93a732ac11b0b3a29d94b64fb8595ad0d87f15ce
RickVellingaa <rickvellinga2408@gmail.com>
2023-03-15 17:45:14 +0100

    huidige pagina class
    active class toegevoegd op huidige pagina

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,10 @@
 const guidelineBtn = document.querySelectorAll('.guidelines__btn')
 const guidelineInfo = document.querySelectorAll('.principleInfo')
 
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('a');
+const menuLength = menuItem.length; 
+
 guidelineBtn.forEach(item => {
     item.addEventListener('click', () => {
         const id = item.dataset.id
@@ -12,3 +16,11 @@ guidelineBtn.forEach(item => {
     }) 
   
 });
+
+for (let i = 0; i<menuLength; i++) {
+    // console.log(menuItem[i].href);
+    if(menuItem[i].href === currentLocation) {
+        menuItem[i].className = "active "
+    }
+}
+
