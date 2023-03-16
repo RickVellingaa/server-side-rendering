7a15af20143a462274c00a4674e80feceffff031
RickVellingaa <rickvellinga2408@gmail.com>
2023-03-16 18:30:53 +0100

    probeer v1

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,9 @@
 const guidelineBtn = document.querySelectorAll('.guidelines__btn')
 const guidelineInfo = document.querySelectorAll('.principleInfo')
 
+const criteriaBtn = document.querySelectorAll('.guidelines__btn1')
+const criteraInfo = document.querySelectorAll('.principleInfo1')
+
 const currentLocation = location.href;
 const menuItem = document.querySelectorAll('a');
 const menuLength = menuItem.length;
@@ -16,6 +19,19 @@ guidelineBtn.forEach(item => {
     })
 
 });
+
+criteriaBtn.forEach(item => {
+    item.addEventListener('click', () => {
+        const id = item.dataset.id
+        const active = document.getElementById(id)
+
+        criteraInfo.forEach(content => content.classList.add('hide1'))
+        active.classList.remove('hide1')
+
+    })
+
+});
+
 
 for (let i = 0; i < menuLength; i++) {
     // console.log(menuItem[i].href);
